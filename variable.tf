cb17d328eaf343b8b05e6979b7ce771827f1a2f8
shadow ridge <shadowridgeroad@gmail.com>
2021-08-11 12:52:36 -0400

    first commit

---
+++ variable.tf
@@ -0,0 +1,4 @@
+variable "ami" { default = "ami-0a313d6098716f372" }
+variable "region" { default = "us-east-1" }
+variable "project" { default = "jenkins-server" }
+variable "mysystem" { default = "173.3.211.214/32" }
